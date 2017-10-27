258e9c8ebfed6ce114b0e44835ccf7918526cbfe
Maxim Salnikov <salnikov@gmail.com>
2017-10-27 06:30:35 +0200

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -8,23 +8,5 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-/*   .then(() => {
-    registerServiceWorker('ngsw-worker')
-  }); */
-
-function registerServiceWorker(swName: string) {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register(`/${swName}.js`)
-      .then(reg => {
-        console.log('[App] Successful service worker registration', reg);
-      })
-      .catch(err =>
-        console.error('[App] Service worker registration failed', err)
-      );
-  } else {
-    console.error('[App] Service Worker API is not supported in current browser');
-  }
-}
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
